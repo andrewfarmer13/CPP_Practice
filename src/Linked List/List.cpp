736b90d83a0c217dce6c4c69fef4d66e0e33a91c
andrewfarmer13 <andrewfarmer13@gmail.com>
2022-10-13 12:09:19 -0500

    Add comments

--- src/Linked List/List.cpp
+++ src/Linked List/List.cpp
@@ -19,23 +19,23 @@ void List::insert(int n) {
 	}
 	else {
 
-		Node* preptr = first;
+		Node* preptr = first;//Creates a new nodeptr to navigate through the list
 
-		while (preptr->next != NULL) {
-			preptr = preptr->next;
+		while (preptr->next != NULL) { //Searches the linked list; if there is data in the next pointer the navigating ptr (preptr) = the next pointer
+			preptr = preptr->next; //The assignment mentioned in the previous comment
 		}
-		newptr->next = preptr->next;
-		preptr->next = newptr;
+		newptr->next = preptr->next;//Sets the newptr->next to Null
+		preptr->next = newptr; //This assings new data the list
 
 	}
 }
 
 void List::display() {
 
-	Node* temp = first;
-	while (temp != NULL) {
-		std::cout << temp->data << ", ";
-		temp = temp->next;
+	Node* temp = first;//Navigating ptr
+	while (temp != NULL) { //While loop that navigates through the linked list while there is still data
+		std::cout << temp->data << ", "; //Prints out the data
+		temp = temp->next; //sets the navigating ptr to the next item in the list
 	}
 	std::cout << "\n";
 }
